d9436fb876dc2a4f883664439604a80bb18c0e7d
joeun9020 <70720350+joeun9020@users.noreply.github.com>
2023-03-16 21:41:17 +0900

    Update 01.py

--- 9mean/이코테파이썬/1주차/01.py
+++ 9mean/이코테파이썬/1주차/01.py
@@ -1,1 +1,11 @@
+#큰수의 법칙
+n, m, k = map(int, input(" ").split())
+index = list(map(int, input(" ").split()))
 
+index.sort(reverse=True)
+a = index[0]
+b = index[1]
+
+#sum = 0
+sum = (m // (k + 1)) * (k * a + b) + (m % (k + 1)) * a
+print(sum)
