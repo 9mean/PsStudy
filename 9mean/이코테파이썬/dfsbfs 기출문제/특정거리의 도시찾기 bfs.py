b4ab1bbb19788098a2517e7741f8c9631f10a9f9
9mean <kanggm102@naver.com>
2023-04-08 00:58:03 +0900

    dfs/bfs 커밋1

---
+++ 9mean/이코테파이썬/dfsbfs 기출문제/특정거리의 도시찾기 bfs.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+# N 도시 수, M 도로 수, K 거리 정보 X 출발 도시
+N, M, K, X = map(int, input().split(' '))
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+  a, b =  map(int, input().split(' '))  
+  graph[a].append(b)
+
+distance = [-1] *(N+1)
+distance[X] = 0
+
+# BFS 부분
+q = deque([X])
+while q:
+  now = q.popleft()
+
+  for next in graph[now]:
+    if distance[next] == -1:
+      distance[next] = distance[now]+1
+      q.append(next)
+
+# K값이 distance에 있다면 i값출력 없다면 -1 출력
+if K in distance:
+  for i in range(1, N+1):
+    if distance[i] == K:
+      print(i)
+      check = True
+else:
+  print(-1)
