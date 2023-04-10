92bafef876b94cb0ff2c44178d7c538b3bd137cb
9mean <kanggm102@naver.com>
2023-04-10 21:23:07 +0900

    연구소 커밋

--- 9mean/이코테파이썬/dfsbfs 기출문제/연구소.py
+++ 9mean/이코테파이썬/dfsbfs 기출문제/연구소.py
@@ -1,3 +1,51 @@
-Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+#왜 pypy3에선 통과하고 python3에선 시간초과
+from collections import deque
+import copy
+
+n,m=map(int,input().split())
+lab=[]
+for i in range(n):
+    lab.append(list(map(int,input().split())))
+
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
+
+def bfs():
+    queue=deque()
+    clab=copy.deepcopy(lab)
+    for i in range(n):
+        for j in range(m):
+            if clab[i][j]==2:
+                queue.append((i,j))
+
+    while queue:
+        x,y=queue.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx>=0 and nx<n and ny>=0 and ny<m:
+                if clab[nx][ny]==0:
+                    clab[nx][ny]=2
+                    queue.append((nx,ny))
+
+    global res
+    cnt=0
+    for i in range(n):
+        cnt+=clab[i].count(0)
+        
+    res=max(res,cnt)
+
+def makeWall(cnt):
+    if cnt==3:
+        bfs()
+        return
+    for i in range(n):
+        for j in range(m):
+            if lab[i][j]==0:
+                lab[i][j]=1
+                makeWall(cnt+1)
+                lab[i][j]=0
+
+res=0
+makeWall(0)
+print(res)
